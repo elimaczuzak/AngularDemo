ae01e05629818d5fcb59d25d1fdfe72b943106b6
Eli Maczuzak <eli.maczuzak@wisc.edu>
2016-06-06 11:31:45 -0500

    Split app component to make file structure more modular

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,12 @@
 import {Component} from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
-export class Hero {
-    id:number;
-    name:string;
-}
-
-
+//noinspection TypeScriptValidateTypes
 @Component({
     selector: 'my-app',
     template: `
     <h1>{{title}}</h1>
-    
     <h2>My Heroes</h2>
     <ul class="heroes">
         <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
@@ -18,19 +14,8 @@ export class Hero {
         </li>
     </ul>
     
-    <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-    </div>
-    </div>
-    
-    <div *ngIf="!selectedHero">
-        Nothing to see here!
-    </div>
-    `
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    `,
 
     styles:[`
   .selected {
@@ -80,7 +65,8 @@ export class Hero {
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+    directives: [HeroDetailComponent]
 })
 
 
